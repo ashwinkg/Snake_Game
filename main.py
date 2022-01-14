1cd4be40ed86b1cd5af9cac1597a64f64f96210c
Ashwin Kadammaje Giridhar <ashu.ashwinkg@gmail.com>
2022-01-13 19:18:18 -0600

    Third Commit

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ while game_is_on:
         game_is_on = False
         scoreboard.display_game_over()
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # using slicing
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.display_game_over()
 
